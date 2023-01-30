1005e18cf04686a8da2db235d0444e8f66d4312e
MrSneakyFox <pepijn222real@gmail.com>
2023-01-30 13:48:16 +0100

    V0.0.6

---
+++ PortfolioWebsite/Contact.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  // Validate form data
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo 'Invalid email';
+    exit;
+  }
+
+  // Send email
+  $to = 'pepijn222real@gmail.com';
+  $subject = 'New message from ' . $name;
+  $headers = 'From: ' . $email . "\r\n" .
+    'Reply-To: ' . $email . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+  mail($to, $subject, $message, $headers);
+
+  echo 'Message sent successfully';
+}
+?>
+
+
+
+
